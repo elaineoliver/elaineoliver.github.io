3f8f62ccaa7c9629e62119651961c0ca36754d8c
Elaine Oliver <e.oliver@elsevier.com>
2024-09-15 19:19:40 +0200

    Trying recommended vite deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://elaineoliver.github.io/",
   plugins: [
     dts({
       tsconfigPath: "./tsconfig.json",
