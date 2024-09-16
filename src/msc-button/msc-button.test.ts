d729efcb5448bb7ec62c7cb457523a51d1878ad2
Elaine Oliver <e.oliver@elsevier.com>
2024-09-16 22:22:21 +0200

    Fixing tests and formatting code

--- src/msc-button/msc-button.test.ts
+++ src/msc-button/msc-button.test.ts
@@ -1,19 +1,19 @@
-import { expect, test } from '@playwright/test';
+import { expect, test } from "@playwright/test";
 
 test.describe("msc-button", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto('index.html');
+    await page.goto("msc.html");
     await page.content();
   });
 
   test(`Emits a button`, async ({ page }) => {
     const button = page.getByRole("button", { name: "Primary button dark" });
-    await button.focus()
+    await button.focus();
     await expect(button).toBeFocused();
-  })
+  });
 
   test(`Looks fabulous`, async ({ page }) => {
     const screenshot = await page.screenshot();
     expect(screenshot).toMatchSnapshot({ maxDiffPixels: 10 });
-  })
-})
+  });
+});
--- src/msc-button/msc-button.ts
+++ src/msc-button/msc-button.ts
@@ -1,12 +1,12 @@
-import { LitElement, css, html } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
+import { LitElement, css, html } from "lit";
+import { customElement, property } from "lit/decorators.js";
 
-@customElement('msc-button')
+@customElement("msc-button")
 export class MSCButton extends LitElement {
   static readonly formAssociated = true;
 
   static override shadowRootOptions: ShadowRootInit = {
-    mode: 'open',
+    mode: "open",
     delegatesFocus: true,
   };
 
@@ -14,11 +14,11 @@ export class MSCButton extends LitElement {
    * Sets the overall style of button.
    * There are no restrictions on the style of the button based on the element it generates.
    */
-  @property({ type: String }) variant?: 'primary' | 'secondary';
+  @property({ type: String }) variant?: "primary" | "secondary";
 
   render() {
     return html`
-      <button class=${this.variant || 'primary'}>
+      <button class=${this.variant || "primary"}>
         <slot name="icon-before"></slot>
         <span class="label">
           <slot></slot>
@@ -26,7 +26,7 @@ export class MSCButton extends LitElement {
         <slot name="icon-after"></slot>
         <slot name="icon-only"></slot>
       </button>
-    `
+    `;
   }
 
   static styles = css`
@@ -35,7 +35,9 @@ export class MSCButton extends LitElement {
       width: fit-content;
     }
 
-    *, *::before, *::after {
+    *,
+    *::before,
+    *::after {
       box-sizing: border-box;
     }
 
@@ -49,11 +51,11 @@ export class MSCButton extends LitElement {
       outline: 2px solid var(--msc-color-interaction-focus);
       outline-offset: 2px;
     }
-    
+
     button:focus-visible:hover {
       outline: none;
     }
-    
+
     button:focus:not(:focus-visible) {
       outline: none;
     }
@@ -98,11 +100,11 @@ export class MSCButton extends LitElement {
       border: 1px solid var(--msc-color-interaction);
       background: var(--msc-color-interaction);
     }
-  `
+  `;
 }
 
 declare global {
   interface HTMLElementTagNameMap {
-    'msc-button': MSCButton
+    "msc-button": MSCButton;
   }
 }
